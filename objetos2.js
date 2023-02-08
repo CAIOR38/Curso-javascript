5d53ff889168e223de6568a31cb881a52152eb98
caior38 <104030451+CAIOR38@users.noreply.github.com>
2023-02-08 20:26:11 -0300

    Add files via upload

---
+++ objetos2.js
@@ -0,0 +1,8 @@
+// Mostrando uma forma de criar objetos através de funçoes.
+console.log(typeof Object);
+console.log(typeof new Object);
+
+const Cliente= function() {
+    console.log(typeof Cliente)
+    console.log(typeof new Cliente)
+}
